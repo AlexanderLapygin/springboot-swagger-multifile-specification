53a3eb68482b9a2fa787c8a4c2ea8a78156edd83
softspider <sftspider@gmail.com>
2023-05-24 14:52:19 +0300

    feat: Swagger settings - for one file so far

---
+++ src/main/java/org/softspiders/config/OpenApiConfig.java
@@ -0,0 +1,8 @@
+package org.softspiders.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    // Дополнительная конфигурация, если необходимо
+}
